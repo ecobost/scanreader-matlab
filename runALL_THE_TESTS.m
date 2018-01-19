4e4b75fda63c94fb22473aa01657d2f7627700d7
Andy Campbell <ranford.b.crandal@gmail.com>
2018-01-19 14:10:29 -0500

    Create runALL_THE_TESTS.m

---
+++ runALL_THE_TESTS.m
@@ -0,0 +1,31 @@
+try
+    import('matlab.unittest.TestRunner');
+    import('matlab.unittest.plugins.XMLPlugin');
+    import('matlab.unittest.plugins.ToFile');
+    import('matlab.unittest.plugins.CodeCoveragePlugin');
+    import('matlab.unittest.plugins.codecoverage.CoberturaFormat');
+
+    ws = getenv('WORKSPACE');
+    
+    suite = testsuite('scanreader');
+
+    % Create and configure the runner
+    runner = TestRunner.withTextOutput('Verbosity',3);
+
+    % Add the XML plugin
+    resultsDir = fullfile(ws, 'testresults');
+    mkdir(resultsDir);
+    
+    resultsFile = fullfile(resultsDir, 'testResults.xml');
+    runner.addPlugin(XMLPlugin.producingJUnitFormat(resultsFile));
+   
+    coverageFile = fullfile(resultsDir, 'cobertura.xml');
+    runner.addPlugin(CodeCoveragePlugin.forPackage('scanreader',...
+        'Producing', CoberturaFormat(coverageFile),'IncludingSubfolders',true));
+ 
+    results = runner.run(suite) 
+catch e
+    disp(getReport(e,'extended'));
+    exit(1);
+end
+quit('force');
