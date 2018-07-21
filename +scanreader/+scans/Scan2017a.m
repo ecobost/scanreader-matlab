304c83e589b32eeb9d41f2d1af9e689d6384b28e
ecobost <emcobost@gmail.com>
2018-07-21 04:45:50 -0500

    Added SI 2018a

--- +scanreader/+scans/Scan2017a.m
+++ +scanreader/+scans/Scan2017a.m
@@ -1,4 +1,3 @@
 classdef Scan2017a < scanreader.scans.Scan5Point3
     %SCAN2017A ScanImage 2017a
 end
-
---
+++ +scanreader/+scans/Scan2018a.m
@@ -0,0 +1,3 @@
+classdef Scan2018a < scanreader.scans.Scan5Point3
+    %SCAN2018A ScanImage 2018a
+end
